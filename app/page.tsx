a16a844a2dcc443d8c07a43cf9011bfa67afbcb5
Mohamad Hosein <fatherofgod1332@gmail.com>
2024-04-08 23:23:32 +0330

    add HomeProperties component for HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
 import Footer from "@/components/Footer";
+import HomeProperties from "@/components/HomeProperties";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <HomeProperties />
       <Footer />
     </>
   );
